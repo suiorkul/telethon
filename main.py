469a84dd10624b3b9f143429ec352b652d79ee98
suiorkul <suerkul5555@gmail.com>
2023-06-01 11:21:31 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import vk_api
+
+
+
+token="vk1.a.M7qDv3BBuiOJSrZMg6xWpaRTex-u7lPqoSx9gB0rBsW_fXVKIakufv09NkLnb6F8ijnwW9CvIMZJ71fQja9APtq01jVbgSQ3GsdzYx0wbbPYeYWaC02XHSlmC_AUqkHnklPZFZnHAG_7DSAE4T8VS8gN78J6QBb-yKmkv28ca3iiuAFFVDx_B-kNcIbQir6Y2hzDCoWF0lWpYZUHiyVtuw&"
